c8abc2b08d3e8fe5d15348e499f31e6aa2642f27
Sakibur59 <sakiburrahman5978@gmail.com>
2026-04-07 12:17:54 +0600

    Book sorting by pages and rating added

--- src/Components/HomePage/ListedBooks/ListedReadBooks.jsx
+++ src/Components/HomePage/ListedBooks/ListedReadBooks.jsx
@@ -1,10 +1,26 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { bookContext } from "../../BookContext/BookProvider";
 import BookCard from "../../ui/BookCard";
 
-const ListedReadBooks = () => {
+const ListedReadBooks = ({sortingType}) => {
   const { readList } = useContext(bookContext);
-    if(readList.length === 0) {
+
+  const [filterReadList,setFilterReadList]=useState(readList);
+
+  useEffect(()=> {
+    if(sortingType){
+      if(sortingType === 'Pages'){
+        const sortData = [...readList].sort((a,b)=> a.totalPages - b.totalPages);
+        setFilterReadList(sortData);
+      } else if(sortingType === 'Rating') {
+        const sortData = [...readList].sort((a,b)=> a.rating - b.rating);
+        setFilterReadList(sortData);
+      }
+    }
+  })
+
+
+    if(filterReadList.length === 0) {
         return <div className="h-[50vh] flex justify-center items-center bg-gray-100">
             <h2 className="font-bold text-3xl">No Read List Data Found</h2>
         </div>
@@ -14,7 +30,7 @@ const ListedReadBooks = () => {
   return (
     <div className="">
       <div className="grid grid-cols-3 gap-4">
-        {readList.map((book, index) => (
+        {filterReadList.map((book, index) => (
         <BookCard key={index} book={book}></BookCard>
       ))}
       </div>
--- src/Pages/Books/Books.jsx
+++ src/Pages/Books/Books.jsx
@@ -1,32 +1,51 @@
-import React, { useContext } from 'react';
-import { bookContext } from '../../Components/BookContext/BookProvider';
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-import 'react-tabs/style/react-tabs.css';
-import ListedReadBooks from '../../Components/HomePage/ListedBooks/ListedReadBooks';
-import ListedWishlistBooks from '../../Components/HomePage/ListedBooks/ListedWishlistBooks';
+import React, { useContext, useState } from "react";
+import { bookContext } from "../../Components/BookContext/BookProvider";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+import ListedReadBooks from "../../Components/HomePage/ListedBooks/ListedReadBooks";
+import ListedWishlistBooks from "../../Components/HomePage/ListedBooks/ListedWishlistBooks";
 
 const Books = () => {
+  // const {readList} = useContext(bookContext);
+  // console.log(readList);
+  const [sortingType,setSortingType] = useState('')
 
-    // const {readList} = useContext(bookContext);
-    // console.log(readList);
-
-    return (
-        <div className='max-w-300 mx-auto my-5'>
-             <Tabs>
-    <TabList>
-      <Tab>Read List</Tab>
-      <Tab>Wishlist</Tab>
-    </TabList>
-
-    <TabPanel>
-      <ListedReadBooks></ListedReadBooks>
-    </TabPanel>
-    <TabPanel>
-      <ListedWishlistBooks></ListedWishlistBooks>
-    </TabPanel>
-  </Tabs>
+  return (
+    <div className="max-w-300 mx-auto my-5">
+      <div className="flex justify-center items-center py-4">
+        <div className="dropdown dropdown-start">
+          <div tabIndex={0} role="button" className="btn m-1">
+            Click : {sortingType}
+          </div>
+          <ul
+            tabIndex="-1"
+            className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm"
+          >
+            <li onClick={()=>setSortingType('Pages')}>
+              <a>Pages</a>
+            </li>
+            <li onClick={()=>setSortingType('Rating')}>
+              <a>Rating</a>
+            </li>
+          </ul>
         </div>
-    );
+      </div>
+
+      <Tabs>
+        <TabList>
+          <Tab>Read List</Tab>
+          <Tab>Wishlist</Tab>
+        </TabList>
+
+        <TabPanel>
+          <ListedReadBooks sortingType={sortingType}></ListedReadBooks>
+        </TabPanel>
+        <TabPanel>
+          <ListedWishlistBooks sortingType={sortingType}></ListedWishlistBooks>
+        </TabPanel>
+      </Tabs>
+    </div>
+  );
 };
 
 export default Books;
